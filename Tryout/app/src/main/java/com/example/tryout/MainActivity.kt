4da69e150c2962d957e09a7f2d9b9c38c330dfcd
Lihi-Rahamim <li.rahamim@gmail.com>
2021-05-25 14:29:51 +0300

    same

--- Tryout/app/src/main/java/com/example/tryout/MainActivity.kt
+++ Tryout/app/src/main/java/com/example/tryout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tryout
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
@@ -10,14 +11,22 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sp = getSharedPreferences(
+            "Current_status",
+            Context.MODE_PRIVATE
+        ) //YourFileName= Any file name you give it
         val countText: TextView = findViewById(R.id.editTextNumberSigned)
-        countText.text = "0"
-        var count = 0
+        val current_count = sp.getString("count", "0")
+        countText.text = current_count
+        var count : Int = current_count?.toInt() ?: 0
+
         val star : ImageView = findViewById(R.id.imageView6)
         star.visibility = INVISIBLE
         val addButton: Button = findViewById(R.id.button)
@@ -34,6 +43,12 @@ class MainActivity : AppCompatActivity() {
             else
                 star.visibility = INVISIBLE
             countText.text = count.toString()
+            val editor = sp.edit()
+            editor.putString(
+                "count",
+                count.toString()
+            )
+            editor.apply()
         }
         val subButton: Button = findViewById(R.id.button2)
         subButton.setOnClickListener()
@@ -46,7 +61,34 @@ class MainActivity : AppCompatActivity() {
             else
                 star.visibility = INVISIBLE
             countText.text = count.toString()
+            val editor = sp.edit()
+            editor.putString(
+                "count",
+                count.toString()
+            )
+            editor.apply()
         }
+        val resetButton: Button = findViewById(R.id.button3)
+        resetButton.setOnClickListener()
+        {
+            val toast = Toast.makeText(this, "Value Reset complete!",Toast.LENGTH_SHORT)
+            toast.show()
+            count = 0
+            countText.text = count.toString()
+            val editor = sp.edit()
+            editor.putString(
+                "count",
+                count.toString()
+            )
+            editor.apply()
+        }
+
+
+
+
+
+
+
 
     }
 }
